32a8ddc3ab5711b9c1a1a618bb1aa8d859c70e65
A Balaji <amulunarayanabalaji@gmail.com>
2022-10-25 17:23:25 +0530

    code changes

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import './Styles.css'
 import './App.css';
 
+
+const ACTIONS ={
+  ADD_DIGIT :'add-digit',
+  CHOOSE_OPERATION : 'choose-operation',
+  CLEAR : 'clear',
+  DELETE_DIGIT:'delete-digit',
+  EVALUTE:'evalute'
+}
+
+function reducer(state , {type , payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return (
+        ...state,
+         currentOperand: `${currentOperand || "" } ${payload.digit}`,
+      )
+  }
+}
+
 function App() {
+  const [{currentOperand , previousOperand ,operation} , dispatch] =useReducer( reducer ,{})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className='output'>
+        <div className='previous-operand'>{previousOperand} {operation}</div>
+        <div className='current-operand'>{currentOperand}</div>
+      </div>
+
+      <button type="button" className='span-two'>AC</button>
+      <button type="button">DEL</button>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button">*</button>
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button">+</button>
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button">-</button>
+      <button type="button">.</button>
+      <button type="button">0</button>
+      <button type="button">=</button>
     </div>
   );
 }
---
+++ calculator/src/components/Calc.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Calc() {
+  return (
+    <div>Calc</div>
+  )
+}
+
+export default Calc
